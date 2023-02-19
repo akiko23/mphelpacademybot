77918f10a49932ed69cad6c8df6554426967fec3
Dmitriy <101889433+akiko23@users.noreply.github.com>
2023-02-19 12:23:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ import os
 import logging
 from flask import Flask, request
 
-BOT_TOKEN = '5426371103:AAHxzZ4or1O9sIRPpegcx_mCB76R1a0t6OM'
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 bot = Bot(BOT_TOKEN, parse_mode='HTML')
 dp = Dispatcher(bot, storage=MemoryStorage())
@@ -17,10 +17,10 @@ server = Flask(__name__)
 logger = aiogram.dispatcher.middlewares.log
 logger.setLevel(logging.DEBUG)
 
-GROUP_ID = '-1001621949125'
+GROUP_ID = os.environ.get("GROUP_ID")
 APP_URL = 'https://academyhelperbot.herokuapp.com/' + BOT_TOKEN
 
-UKASSA_TOKEN = "381764678:TEST:44230"
-SBER_TOKEN = '401643678:TEST:df285492-0a8c-4c66-9b63-2fa28cb6ec59'
+UKASSA_TOKEN = os.environ.get("UKASSA_TOKEN")
+SBER_TOKEN = os.environ.get("SBER_TOKEN")
 
-p2p = QiwiP2P(auth_key='eyJ2ZXJzaW9uIjoiUDJQIiwiZGF0YSI6eyJwYXlpbl9tZXJjaGFudF9zaXRlX3VpZCI6InNzczVvMC0wMCIsInVzZXJfaWQiOiI3OTg5NzUwNTg2MCIsInNlY3JldCI6ImI2MmE1MWYzYmViOTliZmJjYTAwM2U1YjFiNmM4MmQ1MTQ0Mzg5MGJhZWVlMjAxMmZlZTdhYmExZDFiM2IyMjUifX0=')
+p2p = QiwiP2P(auth_key=os.environ.get("QIWI_AUTH_KEY"))
